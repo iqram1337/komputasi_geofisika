f3bc916ec164a03f30319407cab8b1bb50811e3d
Iqram Haris Fahromi <iqramharisfahromi@yahoo.com>
2021-10-21 16:16:50 +0700

    Update exercise 3.11 (pert 2).py

--- exercise 3.11 (pert 2).py
+++ exercise 3.11 (pert 2).py
@@ -1,12 +1,13 @@
 import numpy as np
 
 ## cara leibniz
-a = 8
+n = int(input('Masukkan N: '))
+print('----------------------------------------------------------')
 ans = 0
 
-for i in range(100+1):
+for i in range(n+1):
     x = (1/((4*i)+1)) * (1/((4*i)+3))
-    x = x*a
+    x = x*8
     ans = ans + x
 
 print('Leibniz Scheme :', ans)
@@ -14,10 +15,9 @@ print('Leibniz Scheme :', ans)
 
 
 ## cara Euler
-a = 6
 ans = 0
 
-for i in range(1, 100+1):
+for i in range(1, n+1):
     x = (1/(i**2))
     x = x*6
     ans = ans + x
